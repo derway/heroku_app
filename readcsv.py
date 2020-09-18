4d317f4441e89c2cc1ea3d07f71c7531089ef63f
derway <derway.jong@gmail.com>
2020-09-18 16:02:48 +0800

    2020-09-18 提交

---
+++ readcsv.py
@@ -0,0 +1,76 @@
+import flask
+from flask import jsonify
+import numpy as np
+import pandas as pd
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+
+# test data
+tpe = {
+    "id": 0,
+    "city_name": "台北",
+    "country_name": "台灣",
+    "is_capital": True,
+    "location": {
+        "longitude": 121.569649,
+        "latitude": 25.036786
+    }
+}
+nyc = {
+    "id": 1,
+    "city_name": "紐約",
+    "country_name": "美國",
+    "is_capital": False,
+    "location": {
+        "longitude": -74.004364,
+        "latitude": 40.710405
+    }
+}
+ldn = {
+    "id": 2,
+    "city_name": "倫敦",
+    "country_name": "英國",
+    "is_capital": True,
+    "location": {
+        "longitude": -0.114089,
+        "latitude": 51.507497
+    }
+}
+cities = [tpe, nyc, ldn]
+
+gapminder = pd.read_csv("gapminder.csv")
+gapminder_list = []
+nrows = gapminder.shape[0]
+for i in range(nrows):
+    ser = gapminder.loc[i, :]
+    row_dict = {}
+    for idx, val in zip(ser.index, ser.values):
+        if type(val) is str:
+            row_dict[idx] = val
+        elif type(val) is np.int64:
+            row_dict[idx] = int(val)
+        elif type(val) is np.float64:
+            row_dict[idx] = float(val)
+    gapminder_list.append(row_dict)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1>Hello Flask!</h1> <br> \
+    <a href="/cities">cities</a> <br>\
+    <a href="/gapminder">gapminder</a>'
+
+
+@app.route('/cities', methods=['GET'])
+def cities_all():
+#    return '<a href="/">home</a> <br> \
+    return jsonify(cities)
+
+@app.route('/gapminder', methods=['GET'])
+def gapminder_all():
+    return jsonify(gapminder_list)
+
+
+app.run()
